836a4bf8f0112ac8e6008860a1f154bf1c21f35c
sparshturkane <rajaturkane99@gmail.com>
2017-09-05 11:35:56 +0530

    47 adding content to recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+    selector: 'app-recipe-list',
+    templateUrl: './recipe-list.component.html',
+    styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+        new Recipe(
+            'Recipe Name',
+            'Recipe description',
+            'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'
+        ),
+        new Recipe(
+            'Recipe Name',
+            'Recipe description',
+            'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'
+        ),
+        new Recipe(
+            'Recipe Name',
+            'Recipe description',
+            'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'
+        )
+    ]
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
 }
+
