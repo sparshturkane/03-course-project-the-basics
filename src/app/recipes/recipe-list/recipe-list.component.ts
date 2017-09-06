543b0bb186805ad582b9f0706b3a2097bc51480f
sparshturkane <rajaturkane99@gmail.com>
2017-09-06 12:45:29 +0530

    79 on clicking recipe display data in recipe description

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -9,24 +9,30 @@ import { Recipe } from "../recipe.model";
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
         new Recipe(
-            'Recipe Name',
-            'Recipe description',
+            'Dosa',
+            'Dosa description',
             'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'
         ),
         new Recipe(
-            'Recipe Name',
-            'Recipe description',
+            'Idli',
+            'Idli description',
             'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'
         ),
         new Recipe(
-            'Recipe Name',
-            'Recipe description',
+            'Biriyani',
+            'Biriyani description',
             'http://media2.sailusfood.com/wp-content/uploads/2016/03/recipe-of-momos.jpg'
         )
     ]
+
+    @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter();
     constructor() {}
 
     ngOnInit() {}
+
+    OnRecipeSelected(recipe: Recipe){
+        this.recipeWasSelected.emit(recipe);
+    }
 
 }
 
